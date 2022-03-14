4a62fdb68b47f44062a30166532fac76a2120dc8
gizliadam745 <94307007+gizliadam745@users.noreply.github.com>
2022-03-14 15:50:49 +0400

    Create pw4_ex1.c

---
+++ pw4_ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define ROWS 6
+#define COLUMNS 7
+
+int main(void)
+{
+    // " " = no chips 
+    // "*" = red disc 
+    // "o" = yellow disc
+    char Board[ROWS][COLUMNS] = {
+                                {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                                {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                                {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                                {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                                {' ', ' ', ' ', ' ', ' ', ' ', ' '},
+                                {' ', ' ', ' ', ' ', ' ', ' ', ' '}
+                                };
+
+}
